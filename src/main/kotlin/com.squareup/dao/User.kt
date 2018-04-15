4f60810536a67878734c3af3c492eb1be0871aae
Mrman <mrman2289@gmail.com>
2018-04-15 17:52:41 +0100

    Add type alias to user to avoid classnames in mongo

--- src/main/kotlin/com.squareup/dao/User.kt
+++ src/main/kotlin/com.squareup/dao/User.kt
@@ -1,7 +1,9 @@
 package com.squareup.dao
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.annotation.TypeAlias
 
+@TypeAlias("user")
 data class User(
         @Id val id: String,
         val firstName: String,
