15501f8262703ff08e75782af91fc8eb98922745
Mrman <mrman2289@gmail.com>
2018-04-15 20:55:01 +0100

    Remove testing initialisation of Mongo

--- src/main/kotlin/com.squareup/app/SquareUpApp.kt
+++ src/main/kotlin/com.squareup/app/SquareUpApp.kt
@@ -1,24 +1,10 @@
 package com.squareup.app
 
-import com.squareup.app.dao.User
-import com.squareup.app.repository.UserRepository
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.ApplicationArguments
-import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class SquareUpApp : ApplicationRunner {
-
-    @Autowired
-    lateinit var userRepository : UserRepository
-
-    override fun run(args: ApplicationArguments?) {
-        userRepository.deleteAll()
-        userRepository.save(User("test@square-up.co.uk", "Jim", "Bean"))
-    }
-}
+class SquareUpApp
 
 fun main(args: Array<String>) {
     SpringApplication.run(SquareUpApp::class.java, *args)
