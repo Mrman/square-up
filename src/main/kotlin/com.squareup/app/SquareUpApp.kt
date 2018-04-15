f93d1ba2f13acf117f24d431616b25c5c30fcf62
Mrman <mrman2289@gmail.com>
2018-04-15 18:39:27 +0100

    Temporarily autowire a repo and create a user to test connectivity

--- src/main/kotlin/com.squareup/app/SquareUpApp.kt
+++ src/main/kotlin/com.squareup/app/SquareUpApp.kt
@@ -1,11 +1,23 @@
 package com.squareup.app
 
+import com.squareup.app.dao.User
+import com.squareup.app.repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class SquareUpApp {
+class SquareUpApp : ApplicationRunner {
 
+    @Autowired
+    lateinit var userRepository : UserRepository
+
+    override fun run(args: ApplicationArguments?) {
+        userRepository.deleteAll()
+        userRepository.save(User("test@square-up.co.uk", "Jim", "Bean"))
+    }
 }
 
 fun main(args: Array<String>) {
