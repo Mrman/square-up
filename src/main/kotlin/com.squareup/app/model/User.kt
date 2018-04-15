4d4508b8148af478236fd413b134153bca1fb7ab
Mrman <mrman2289@gmail.com>
2018-04-15 20:54:09 +0100

    Remove unnecessary JsonCreator annotation usage

--- src/main/kotlin/com.squareup/app/model/User.kt
+++ src/main/kotlin/com.squareup/app/model/User.kt
@@ -1,7 +1,5 @@
 package com.squareup.app.model
 
-import com.fasterxml.jackson.annotation.JsonCreator
-
-data class User @JsonCreator constructor(val emailAddress: String, val firstName: String, val lastName: String) {
+data class User constructor(val emailAddress: String, val firstName: String, val lastName: String) {
     fun toDao(): com.squareup.app.dao.User = com.squareup.app.dao.User(emailAddress, firstName, lastName)
 }
