8ae68c7cd94232701c1ff6395a344b44bc5669e7
Mrman <mrman2289@gmail.com>
2018-04-22 22:06:12 +0100

    Email address could be null if user hasn't given this access in app signup

--- src/main/kotlin/com.squareup/app/facebook/model/FacebookUser.kt
+++ src/main/kotlin/com.squareup/app/facebook/model/FacebookUser.kt
@@ -8,7 +8,7 @@ data class FacebookUser (
         @JsonProperty(FacebookUserField.RawFieldNames.USER_ID) val facebookId: String,
         @JsonProperty(FacebookUserField.RawFieldNames.FIRST_NAME) val firstName: String,
         @JsonProperty(FacebookUserField.RawFieldNames.LAST_NAME) val lastName: String,
-        @JsonProperty(FacebookUserField.RawFieldNames.EMAIL_ADDRESS) val emailAddress: String,
+        @JsonProperty(FacebookUserField.RawFieldNames.EMAIL_ADDRESS) val emailAddress: String?,
         private val additionalProperties: MutableMap<String, Any> = mutableMapOf()
 ) {
     @JsonAnyGetter
